ec022a2c8d75a42228bd4466186424188ecac4bb
kobellem <kobellem@infogroep.be>
2017-04-15 17:59:37 +0200

    store benchmark results in benchmark folder

--- src/run_benchmarks.sh
+++ src/run_benchmarks.sh
@@ -2,11 +2,11 @@ for i in {1..64}
 do
     echo "---"
     echo "> fib, $i threads"
-    erl +S $i -noshell -s benchmark test_fib -s init stop > output-fib-$i.txt
+    erl +S $i -noshell -s benchmark test_fib -s init stop > benchmarks/output-fib-$i.txt
     echo "---"
     echo "> get_channel_history, $i threads"
-    erl +S $i -noshell -s benchmark test_get_channel_history -s init stop > output-get_channel_history-$i.txt
+    erl +S $i -noshell -s benchmark test_get_channel_history -s init stop > benchmarks/output-get_channel_history-$i.txt
     echo "---"
     echo "> send_message, $i threads"
-    erl +S $i -noshell -s benchmark test_send_message -s init stop > output-send_message-$i.txt
+    erl +S $i -noshell -s benchmark test_send_message -s init stop > benchmarks/output-send_message-$i.txt
 done
